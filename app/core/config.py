4ca2c0f5d8ddaa0a39d57e2f0d4310209a9c3d38
zvekon.na <zvekov.na@esphere.ru>
2023-03-18 13:49:39 +0300

    Edit README.md

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_title: str
-    description: str
+    app_title: str = 'Кошачий благотворительный фонд'
+    description: str = 'Сервис для поддержки котиков!'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
 
