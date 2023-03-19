9410c9bb4bfdd771291995a22587e4aa010210ed
zvekon.na <zvekov.na@esphere.ru>
2023-03-19 10:36:26 +0300

    Исправления замечаний по ревью

--- app/services/investment.py
+++ app/services/investment.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import select
+from sqlalchemy import false, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import CharityProject, Donation
@@ -13,11 +13,11 @@ async def get_not_invested_data(
     Получение из БД первых по очереди незакрытых проектов и пожертвований.
     """
     project = await session.execute(select(CharityProject).where(
-        CharityProject.fully_invested == 0
+        CharityProject.fully_invested == false()
     ).order_by('create_date'))
     project = project.scalars().first()
     donation = await session.execute(select(Donation).where(
-        Donation.fully_invested == 0
+        Donation.fully_invested == false()
     ).order_by('create_date'))
     donation = donation.scalars().first()
     return project, donation
@@ -32,6 +32,7 @@ async def investment_process(
     if not project or not donation:
         return obj
 
+    close_date = datetime.now()
     balance_project = project.full_amount - project.invested_amount
     balance_donation = donation.full_amount - donation.invested_amount
 
@@ -39,19 +40,19 @@ async def investment_process(
         project.invested_amount += balance_donation
         donation.invested_amount += balance_donation
         donation.fully_invested = True
-        donation.close_date = datetime.now()
+        donation.close_date = close_date
     elif balance_project == balance_donation:
         project.invested_amount += balance_donation
         donation.invested_amount += balance_donation
         project.fully_invested = True
         donation.fully_invested = True
-        project.close_date = datetime.now()
-        donation.close_date = datetime.now()
+        project.close_date = close_date
+        donation.close_date = close_date
     else:
         project.invested_amount += balance_project
         donation.invested_amount += balance_project
         project.fully_invested = True
-        project.close_date = datetime.now()
+        project.close_date = close_date
 
     session.add(project)
     session.add(donation)
