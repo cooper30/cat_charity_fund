744d3575bf1ab97fb21ddf9e473ed03356177fb3
Nikita Zvekov <35966513+cooper30@users.noreply.github.com>
2024-08-16 16:41:03 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI
 from app.api.routers import main_router
 from app.core.config import settings
 
-app = FastAPI()
-
 app = FastAPI(title=settings.app_title, description=settings.description)
 
 app.include_router(main_router)
